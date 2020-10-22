6d2cb57be77f8fc1ba9384df3e7157f06d37fbba
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-22 23:19:30 +0200

    Code nog meer persoonlijk opgevoerd met gebruikersinvoer! Volgens mij heb ik de invoer-errors hiermee opgelost: eureka!

---
+++ src/Auto.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+public class Auto {
+    // Definiëren: Variabelen
+    static Scanner invoer = new Scanner(System.in);
+    private int snelheid;
+    private String merk;
+    private int kilometerstand;
+    // Einde Definiëren: Variabelen
+
+    public static void main(String[] args) { // Main
+
+        Auto auto = new Auto("Toyota", 100, 12000);
+        System.out.print("\nSnelheid: " + auto.snelheid);
+        System.out.print("\nMerk: " + auto.merk);
+        System.out.print("\nKilometerstand: " + auto.kilometerstand);
+        System.out.println();
+        Auto.motorGegevens();
+
+        System.out.print("\n―――――――――――――――――――――――――\n");
+        Auto Tesla = new Auto("Tesla", 180, 22000);
+//        Tesla.setMerk("Tesla");
+        System.out.print("\nSnelheid: " + Tesla.snelheid);
+        System.out.print("\nMerk: " + Tesla.merk);
+        System.out.print("\nKilometerstand: " + Tesla.kilometerstand);
+        System.out.println();
+        Tesla.motorGegevens();
+
+        System.out.print("\n―――――――――――――――――――――――――\n");
+        System.out.print("\nVoer merk in: ");
+        String invoerMerk = invoer.nextLine();
+
+        System.out.print("\nVoer snelheid als getallen in: ");
+        boolean checkInvoer = false;
+        int invoerSnelheid = 0;
+        while (!checkInvoer) {
+            try {
+                invoerSnelheid = Integer.parseInt(invoer.nextLine());
+                checkInvoer = true;
+            } catch (NumberFormatException e) {
+                System.out.print("\nIk zei toch dat je als getallen moest invoeren! Voer nogmaals snelheid als getallen in: ");
+                checkInvoer = false;
+            }
+        }
+
+        checkInvoer = false;
+
+        System.out.print("\nVoer kilometerstand als getallen in: ");
+        int invoerKilometerStand = 0;
+        while (!checkInvoer) {
+            try {
+                invoerKilometerStand = Integer.parseInt(invoer.nextLine());
+                checkInvoer = true;
+            } catch (NumberFormatException e) {
+                System.out.print("\nZucht, nogmaals, gaarne als getallen invoeren! Voer nogmaals kilometerstand als getallen in: ");
+                checkInvoer = false;
+            }
+        }
+
+        Auto autoInvoer = new Auto(invoerMerk, invoerSnelheid, invoerKilometerStand);
+
+        System.out.print("\nSnelheid: " + autoInvoer.snelheid);
+        System.out.print("\nMerk: " + autoInvoer.merk);
+        System.out.print("\nKilometerstand: " + autoInvoer.kilometerstand);
+        System.out.println();
+        autoInvoer.motorGegevens();
+
+
+    } // Einde Main
+
+//    public Auto() {
+//        snelheid = 0;
+//        merk = "Toyota";
+//        kilometerstand = 20000;
+//    }
+
+    // Constructor Auto
+    public Auto(String merk, int snelheid, int kilometerstand) {
+        this.merk = merk;
+        this.snelheid = snelheid;
+        this.kilometerstand = kilometerstand;
+
+//        System.out.print("\nVoer merk in: ");
+//        this.merk = invoer.nextLine();
+
+//        setMerk("merk");
+    } // Einde Constructor Auto
+
+    // Definiëren: motorGegevens
+    public static String motorGegevens() {
+        String motorGegevens = "Motor: Colombo V12 580 pk 980 kilo 3,8 liter";
+
+        System.out.println(motorGegevens);
+
+        return motorGegevens;
+    } // Einde motorGegevens
+
+    // Definiëren: Accessor & Mutator: snelheid
+    public void setSnelheid(int snelheid) { // Accessor: snelheid
+        this.snelheid = snelheid;
+    }
+
+    public int getSnelheid() { // Mutator: snelheid
+        return snelheid;
+    } // Einde Definiëren: Accessor & Mutator: snelheid
+
+
+    // Definiëren: Accessor & Mutator: merk
+    public String getMerk() { // Accessor: merk
+        return merk;
+    }
+
+    public void setMerk(String merk) { // Mutator: merk
+        this.merk = merk;
+    } // Einde Definiëren: Accessor & Mutator: merk
+
+
+    // Definiëren: Accessor & Mutator: kilometerstand
+    public int getKilometerstand() { // Accessor: kilometerstand
+        return kilometerstand;
+    }
+
+    public void setKilometerstand(int kilometerstand) { // Mutator: kilometerstand
+        this.kilometerstand = kilometerstand;
+    } // Instellen: Accessor & Mutator: kilometerstand
+}
+
